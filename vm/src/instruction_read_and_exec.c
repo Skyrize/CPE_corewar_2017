6fc41c58b3ecb8eee41bfbd91e1ad19052756030
Clément <clement.baudonnel@epitech.eu>
2018-03-01 21:32:56 +0100

    merge

--- vm/src/instruction_read_and_exec.c
+++ vm/src/instruction_read_and_exec.c
@@ -20,6 +20,7 @@ int execute_instruct(pc_t *pc, char *vm, champ_t *champ)
 	int op = get_op(vm[pc->idx]);
 	byte **tab;
 
+	check_functions_args_and_bytes(op_tab[op], pc, vm);
 	//op_tab[op].fptr(champ, pc, tab);
 	pc->countdown = op_tab[op].nbr_cycles;
 	return (0);
